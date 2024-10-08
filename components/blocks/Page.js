730b640c0012e0b1fa6571db9f6e4b8ebc61d31a
leahtimmins <leahtimmins2@gmail.com>
2024-10-08 23:29:01 +0100

    updated PageTitle component. Added TextImageGrid component.

--- components/blocks/Page.js
+++ components/blocks/Page.js
@@ -2,7 +2,7 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 
 const Page = ({ blok }) => {
     return (
-        <main {...storyblokEditable(blok)}>
+        <main {...storyblokEditable(blok)} className="pt-20">
             {blok.body.map((nestedBlok) => (
             <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
             ))}
---
+++ components/blocks/TextImageGrid.js
@@ -0,0 +1,38 @@
+'use client'
+
+import Container from "@/components/elements/Container";
+import clsx from 'clsx';
+import RichText from "../elements/RichText";
+import dimensions from "@/utils/dimensions";
+import Image from 'next/image';
+
+const TextImageGrid = ({blok, position}) => {
+    const {
+        background,
+        textColor,
+        imageFirst,
+        image,
+        richText,
+        _uid
+    } = blok;
+
+    let size = dimensions(image.filename);
+
+    return(
+        <div id={_uid} data-name="text-image-grid" className={clsx("relative py-8", background, textColor)}>
+            <Container>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                    <div className={`${imageFirst == true ? "order-1 lg:order-2" : "order-1"}`}>
+                        {richText && <RichText story={richText} className="mt-6 text-lg leading-8 text-gray-600" />}
+                    </div>
+                    <div className={`${imageFirst == true ? "order-2 lg:order-1" : "order-2"}`}>
+
+                        <Image src={image.filename} alt={image.alt} width={size.width} height={size.height} />
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default TextImageGrid;
