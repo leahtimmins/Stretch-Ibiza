c7da0b1244bd1942c11d8da5a679532b357af7e9
leahtimmins <leahtimmins2@gmail.com>
2024-10-15 23:21:40 +0100

    update plain text component to add image to serializer

--- components/blocks/PlainText.js
+++ components/blocks/PlainText.js
@@ -1,5 +1,7 @@
 import Container from "../elements/Container";
-import { render, NODE_PARAGRAPH, MARK_LINK } from 'storyblok-rich-text-react-renderer';
+import { render, NODE_PARAGRAPH, MARK_LINK, NODE_IMAGE } from 'storyblok-rich-text-react-renderer';
+import Link from 'next/link';
+import dimensions from "@/utils/dimensions";
 
 const PlainText = ({blok}) => {
     const {
@@ -16,21 +18,26 @@ const PlainText = ({blok}) => {
                 <div className="max-w-5xl mx-auto">
                     {render(richText, {
                         nodeResolvers: {
-                            [NODE_PARAGRAPH]: (children, props) => <p className="mb-4 text-lg">{children}</p>
+                            [NODE_PARAGRAPH]: (children, props) => { return (<p className="mb-4 text-lg">{children}</p>)},
+                            [NODE_IMAGE]: (children, { src, alt }) => { 
+                                
+                                const imageSize = dimensions(src);
+                                return (<img src={src} alt={alt} width={imageSize.width} height={imageSize.height} className="max-h-36 w-auto mx-auto mt-8" />)
+                            }
                         },
                         markResolvers: {
                             [MARK_LINK]: (children, props) => {
                                 const { linktype, href, target } = props;
                                 if (linktype === 'email') {
                                     // Email links: add `mailto:` scheme and map to <a>
-                                    return <a href={`mailto:${href}`}>{children}</a>;
+                                    return <a href={`mailto:${href}`} className="underline">{children}</a>;
                                 }
                                 if (href.match(/^(https?:)?\/\//)) {
                                     // External links: map to <a>
-                                    return <a href={href} target={target}>{children}</a>;
+                                    return <a href={href} target={target} className="underline">{children}</a>;
                                 }
                                 // Internal links: map to <Link>
-                                return <Link href={href}>{children}</Link>;
+                                return <Link href={href} className="underline">{children}</Link>;
                             }
                         }
                     })}
