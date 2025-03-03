85c2c806885be09b599b97b739e6160946032830
leahtimmins <leahtimmins2@gmail.com>
2025-03-03 11:36:39 +0000

    added mcjs script in a different way.

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Footer from "@/components/blocks/Footer";
 import Header from "@/components/blocks/Header";
 import { Suspense } from 'react';
-import { storyblokInit, apiPlugin} from "@storyblok/react/rsc"
+import { storyblokInit, apiPlugin } from "@storyblok/react/rsc"
 import StoryblokProvider from "@/lib/StoryBlokProvider";
 import { fetchSettings } from "@/lib/StoryBlok";
 import { SettingsProvider } from "@/components/context/Settings";
@@ -50,33 +50,35 @@ export default async function RootLayout({ children }) {
 
   return (
     <StoryblokProvider>
-      
-        <html lang="en" className="w-screen">
-          <body
-            className={`${glacialRegular.className} ${glacialItalic.className} ${glacialBold.className} antialiased w-full`}
-          >
-            <Script
-              id="mcjs"
-              strategy="beforeInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `!function(c,h,i,m,p){
-                  m=c.createElement(h),
-                  p=c.getElementsByTagName(h)[0],
-                  m.async=1,
-                  m.src=i,
-                  p.parentNode.insertBefore(m,p)
-                }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/57d0c9b60c47da3fb219b36cd/7ea86ea1a0f0b85649fc5b2c8.js");`,
-              }}
-            />
 
-            <SettingsProvider settings={settings}>
-              <Header />
-              <Suspense>{children}</Suspense>
-              <Footer />
-            </SettingsProvider>
-            
-          </body>
-        </html>
+      <html lang="en" className="w-screen">
+        <head>
+          <Script
+            id="mcjs"
+            strategy="afterInteractive" // Ensures the script loads after page hydration
+            dangerouslySetInnerHTML={{
+              __html: `!function(c,h,i,m,p){
+              m=c.createElement(h),
+              p=c.getElementsByTagName(h)[0],
+              m.async=1,
+              m.src=i,
+              p.parentNode.insertBefore(m,p)
+            }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/57d0c9b60c47da3fb219b36cd/7ea86ea1a0f0b85649fc5b2c8.js");`,
+            }}
+          />
+        </head>
+        <body
+          className={`${glacialRegular.className} ${glacialItalic.className} ${glacialBold.className} antialiased w-full`}
+        >
+
+          <SettingsProvider settings={settings}>
+            <Header />
+            <Suspense>{children}</Suspense>
+            <Footer />
+          </SettingsProvider>
+
+        </body>
+      </html>
     </StoryblokProvider>
   );
 }
