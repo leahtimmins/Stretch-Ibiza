a8e9c6b8f8688b8c4275a2806d053efa4660f09a
leahtimmins <leahtimmins2@gmail.com>
2025-09-28 17:15:41 +0100

    Update body text padding

--- components/blocks/BodyText.js
+++ components/blocks/BodyText.js
@@ -9,17 +9,18 @@ const BodyText = ({blok, position}) => {
         component,
         background,
         richText,
-        anchorIdentity
+        anchorIdentity,
+        padding
     } = blok;
 
 
     return (
         <section id={anchorIdentity ? anchorIdentity : _uid} data-name={component} className={`${background}`}>
-            <div className="py-16">
+            <div className={`${padding == 'small' ? 'py-8' : 'py-16'}`}>
                 <Container>
                    {render(richText, {
                         nodeResolvers: {
-                            [NODE_PARAGRAPH]: (children, props) => <p className="mb-4">{children}</p>
+                            [NODE_PARAGRAPH]: (children, props) => <p className="mb-4 text-lg">{children}</p>
                         },
                         markResolvers: {
                             [MARK_LINK]: (children, props) => {
