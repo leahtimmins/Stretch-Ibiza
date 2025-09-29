5f501aa572bac163c1ca7ac129dad70c52c998ba
leahtimmins <leahtimmins2@gmail.com>
2025-09-29 23:57:15 +0100

    Added ImageBelowImage in wayfinder

--- components/blocks/Wayfinder.js
+++ components/blocks/Wayfinder.js
@@ -14,6 +14,7 @@ const Wayfinder = ({blok, position}) => {
         description,
         background,
         imageBelowText,
+        imageBelowImage,
         setImageFirst,
         sectionPadding,
         contentId,
@@ -25,13 +26,18 @@ const Wayfinder = ({blok, position}) => {
     } = blok;
 
     let imageBelowTextSize = {};
+    let imageBelowImageSize = {};
 
-    if (imageBelowText.filename) {
-        imageBelowTextSize = dimensions(imageBelowText.filename);
+    if (imageBelowText?.filename) {
+        imageBelowTextSize = dimensions(imageBelowText?.filename);
+    }
+
+    if (imageBelowImage?.filename) {
+        imageBelowImageSize = dimensions(imageBelowImage?.filename);
     }
 
     return (
-        <section className={`overflow-hidden ${sectionPadding === 'none' ? "py-0" : sectionPadding === 'small' ? "py-4 sm:py-8 md:py-12" : "py-12 smpy-24 md:py-32"} ${background}`} id={contentId ? contentId : anchorIdentity ? anchorIdentity : _uid} data-name={component}>
+        <section className={`overflow-hidden ${sectionPadding === 'none' ? "py-0" : sectionPadding === 'small' ? "py-4 sm:py-8 md:py-12" : "py-12 sm:py-24 md:py-32"} ${background}`} id={contentId ? contentId : anchorIdentity ? anchorIdentity : _uid} data-name={component}>
             <Container>
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                     <div className={`${setImageFirstMobile ? "order-2" : "order-1"} ${setImageFirst ? "sm:order-2 lg:pl-4 lg:pt-4" : "sm:order-1 lg:pr-4 lg:pt-4"}`}>
@@ -44,12 +50,21 @@ const Wayfinder = ({blok, position}) => {
                                     </div>
                             )} */}
                             {imageBelowText.filename && (
-                                <Image src={`${imageBelowText.filename}`} width={imageBelowTextSize.width} height={imageBelowTextSize.height} alt={image.alt} className="h-36 w-auto mx-auto my-12" />
+                                <Image src={`${imageBelowText?.filename}`} width={imageBelowTextSize?.width} height={imageBelowTextSize?.height} alt={imageBelowText?.alt} className="h-36 w-auto mx-auto my-12" />
                             )}
                         </div>
                     </div>
-                    <div className={`flex items-start ${setImageFirstMobile ? "order-1" : "order-2" } ${setImageFirst ? "sm:order-1 justify-end" : "justify-start sm:order-2"}`}>
-                        <ImageRef image={image} width={600} height={600} className="w-full sm:w-[48rem] max-w-none md:w-[57rem]" />
+                    <div className={`${setImageFirstMobile ? "order-1" : "order-2" } ${setImageFirst ? "sm:order-1 justify-end" : "justify-start sm:order-2"}`}>
+                     
+
+                            <ImageRef image={image} width={600} height={600} className="w-full sm:w-[48rem] max-w-full md:w-[57rem]" />
+                            {imageBelowImage?.filename && (
+                             
+                                <Image src={`${imageBelowImage?.filename}`} width={imageBelowImageSize?.width} height={imageBelowImageSize?.height} alt={imageBelowImage?.alt} className="h-36 w-auto mx-auto my-12" />
+                         
+                            )}
+
+                        
                     </div>
                 </div>
             </Container>
